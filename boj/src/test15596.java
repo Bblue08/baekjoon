c4e3b6561061a5f628f6cf209b22c5e6dd772f4e
jaja8 <jaja8@LG-gram>
2022-02-08 18:51:20 +0900

    test 15596

--- boj/src/test15596.java
+++ boj/src/test15596.java
@@ -1,9 +1,17 @@
-import java.util.*;
+
 public class test15596 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	long sum(int[] a) {
+		long sum=0;
+		
+		for(int i=0;i<a.length;i++) {
+			sum+=a[i];
+		}
+		
+		return sum;
 	}
+	
+
+	
 
 }
+//역시 문제를 끝까지 읽어야한다. 
